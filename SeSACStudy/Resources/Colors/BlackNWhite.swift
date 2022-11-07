4c074e5a5a4f5bbaf39670d1b708b63b29a0cc37
skyler <ho971122@gmail.com>
2022-11-07 17:54:28 +0900

    Assets 추가

---
+++ SeSACStudy/Resources/Colors/BlackNWhite.swift
@@ -0,0 +1,12 @@
+//
+//  BlackNWhite.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/11/07.
+//
+
+import Foundation
+
+struct BlackNWhite {
+    static let white = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+}
---
+++ SeSACStudy/Resources/Colors/SystemColor.swift
@@ -0,0 +1,8 @@
+//
+//  SystemColor.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/11/07.
+//
+
+import Foundation
