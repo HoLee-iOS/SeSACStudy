d8a44efcdf973a8fbe3faa3021dcfef6b0ab71b6
skyler <ho971122@gmail.com>
2022-11-23 18:52:21 +0900

    Feat: 홈화면에 필터 버튼 탭에 대한 뷰모델 제작

--- SeSACStudy/Scenes/Main/Home/HomeViewModel.swift
+++ SeSACStudy/Scenes/Main/Home/HomeViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class HomeViewModel: CommonViewModel {
+    
+    struct Input {
+        let allTap: ControlEvent<Void>
+        let maleTap: ControlEvent<Void>
+        let femaleTap: ControlEvent<Void>
+    }
+    
+    struct Output {
+        let filterTap: Observable<GenderFilterTap>
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        let filterTap = Observable.merge(input.allTap.map{GenderFilterTap.all}, input.maleTap.map{GenderFilterTap.male}, input.femaleTap.map{GenderFilterTap.female})
+        
+        return Output(filterTap: filterTap)
+    }
+}
