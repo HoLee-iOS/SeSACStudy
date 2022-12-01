e030074cea8d89d74f1e287fcf87526771451fa3
skyler <ho971122@gmail.com>
2022-12-01 18:28:23 +0900

    Feat: 새싹 서치 뷰 모델 추가

---
+++ SeSACStudy/Scenes/Main/Home/SesacSearch/SesacSearchViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SesacSearchViewModel.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/11/25.
+//
+
+import Foundation
+
+class SesacSearchViewModel: CommonViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
