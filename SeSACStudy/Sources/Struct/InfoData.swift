a7b12b0624661b4db4bb16de290917c521f9553a
skyler <ho971122@gmail.com>
2022-12-01 18:17:09 +0900

    Feat: 인포데이터 구조체 확인

---
+++ SeSACStudy/Sources/Struct/InfoData.swift
@@ -0,0 +1,18 @@
+//
+//  InfoData.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/11/26.
+//
+
+import UIKit
+
+struct InfoData: Hashable {
+    let profile: UIImage?
+    var name: String?
+    private let id = UUID()
+    
+    static func contents() -> [InfoData] {
+        return [InfoData(profile: Icons.sesacFace1, name: UserDefaultsManager.nickname)]
+    }
+}
