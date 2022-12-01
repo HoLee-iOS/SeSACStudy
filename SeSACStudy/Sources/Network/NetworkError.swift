b7893695694ff4ee13d93882f34f0d77dbbeb2df
skyler <ho971122@gmail.com>
2022-12-01 18:20:34 +0900

    Feat: 네트워크 에러 케이스 추가

--- SeSACStudy/Sources/Network/NetworkError.swift
+++ SeSACStudy/Sources/Network/NetworkError.swift
@@ -11,6 +11,9 @@ enum NetworkError: Int {
     case success = 200
     case alreadySignUp = 201
     case invalidNick = 202
+    case alreadyMatched = 203
+    case penalty2 = 204
+    case penalty3 = 205
     case invalidToken = 401
     case needSignUp = 406
     case serverError = 500
@@ -21,6 +24,9 @@ enum NetworkError: Int {
         case .success: return "로그인 성공"
         case .alreadySignUp: return "이미 가입한 유저"
         case .invalidNick: return "사용할 수 없는 닉네임"
+        case .alreadyMatched: return "이미 매칭된 상태"
+        case .penalty2: return "패널티 2단계"
+        case .penalty3: return "패널티 3단계"
         case .invalidToken: return "토큰 만료"
         case .needSignUp: return "미가입 회원"
         case .serverError: return "서버 에러"
