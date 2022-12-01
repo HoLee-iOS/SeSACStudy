71c5e9ec8c04b738f189d1b203a8750f09e4a959
skyler <ho971122@gmail.com>
2022-12-01 18:19:04 +0900

    Feat: 채팅 화면 추가

---
+++ SeSACStudy/Scenes/Main/Home/Chatting/ChattingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingViewController.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/11/30.
+//
+
+import Foundation
+
+class ChattingViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
