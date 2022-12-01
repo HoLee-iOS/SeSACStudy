6706f6e21d9505eeb3f1578b1e36b0b9bfb3e0e7
skyler <ho971122@gmail.com>
2022-12-01 18:24:18 +0900

    Feat: dummy 데이터 제거

---
+++ SeSACStudy/Sources/Enum/InfoSection.swift
@@ -0,0 +1,19 @@
+//
+//  Section.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/11/15.
+//
+
+import Foundation
+import UIKit
+
+enum InfoSection: Int, Hashable, CaseIterable {
+    case main
+    case profile
+    case gender
+    case study
+    case search
+    case age
+    case withdraw
+}
