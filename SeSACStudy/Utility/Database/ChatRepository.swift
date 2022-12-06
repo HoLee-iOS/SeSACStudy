b695b912cdcd241359556307e25cffb52247301c
skyler <ho971122@gmail.com>
2022-12-06 15:52:23 +0900

    Feat: 채팅 Realm 데이터 객체 싱글턴 파일 추가

---
+++ SeSACStudy/Utility/Database/ChatRepository.swift
@@ -0,0 +1,41 @@
+//
+//  ChatRepository.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/05.
+//
+
+import Foundation
+
+import RealmSwift
+
+class ChatRepository {
+    
+    private init() {}
+    
+    static let shared = ChatRepository()
+    
+    let localRealm = try! Realm()
+    
+    func fetch() -> Results<ChatData> {
+        return localRealm.objects(ChatData.self)
+    }
+    
+    //MARK: - 채팅 저장 기능
+    func sendChat(item: ChatData) {
+        do {
+            try localRealm.write {
+                localRealm.add(item)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    //MARK: - 채팅 삭제 기능
+    func deleteChat(item: ChatData) {
+        try! localRealm.write {
+            localRealm.deleteAll()
+        }
+    }
+}
