d66f0df98cdcd035a9289f75c9775406353de9ab
skyler <ho971122@gmail.com>
2022-12-06 15:51:43 +0900

    Feat: 채팅 Realm 데이터 구조 파일 추가

---
+++ SeSACStudy/Utility/Database/ChatData.swift
@@ -0,0 +1,30 @@
+//
+//  ChatData.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/05.
+//
+
+import Foundation
+import RealmSwift
+
+class ChatData: Object {
+    
+    @Persisted(primaryKey: true) var objectId: ObjectId
+    
+    @Persisted var chatId: String
+    @Persisted var toChat: String
+    @Persisted var fromChat: String
+    @Persisted var chatContent: String
+    @Persisted var chatDate = Date()
+    
+    convenience init(chatId: String, toChat: String, fromChat: String, chatContent: String, chatDate: Date) {
+        self.init()
+        self.chatId = chatId
+        self.toChat = toChat
+        self.fromChat = fromChat
+        self.chatContent = chatContent
+        self.chatDate = chatDate
+    }
+    
+}
