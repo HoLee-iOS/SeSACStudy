20d3142256941f702cc8b9cb7d0503a192504206
skyler <ho971122@gmail.com>
2022-12-06 16:03:53 +0900

    Feat: 더 보기 메뉴 내부에 사용하는 커스텀 버튼 추가

---
+++ SeSACStudy/Sources/Custom/MoreMenuButton.swift
@@ -0,0 +1,35 @@
+//
+//  MoreMenuButton.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/04.
+//
+
+import UIKit
+
+class MoreMenuButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience init(text: String, image: UIImage?) {
+        self.init(frame: .zero)
+        configure(text: text, image: image)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(text: String, image: UIImage?) {
+        var config = UIButton.Configuration.plain()
+        config.attributedTitle = AttributedString.init(text)
+        config.attributedTitle?.font = UIFont(name: Fonts.regular, size: 14)
+        config.attributedTitle?.foregroundColor = BlackNWhite.black
+        config.image = image
+        config.imagePlacement = .top
+        config.imagePadding = 4
+        self.configuration = config
+    }
+}
