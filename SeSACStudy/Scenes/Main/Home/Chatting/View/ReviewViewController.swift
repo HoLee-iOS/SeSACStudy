1cfaee78fb4e12f7c28c449642626a103ccc7efc
skyler <ho971122@gmail.com>
2022-12-12 16:58:55 +0900

    Feat: 리뷰 팝업 화면 뷰컨 생성

---
+++ SeSACStudy/Scenes/Main/Home/Chatting/View/ReviewViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ReviewViewController.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/04.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class ReviewViewController: BaseViewController {
+    
+    let popup = ReviewPopupView()
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func configure() {
+        view.backgroundColor = BlackNWhite.black.withAlphaComponent(0.5)
+        view.addSubview(popup)
+    }
+    
+    override func setConstraints() {
+        popup.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.6)
+            $0.center.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func bindData() {
+        popup.closeButton.rx.tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                vc.dismiss(animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
