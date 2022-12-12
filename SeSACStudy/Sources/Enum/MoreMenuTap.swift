b676e365465330e494a3bd9fdbee7ff5ec34ef53
skyler <ho971122@gmail.com>
2022-12-12 16:37:37 +0900

    Feat: 더 보기 메뉴 탭 열거형 파일 생성

---
+++ SeSACStudy/Sources/Enum/MoreMenuTap.swift
@@ -0,0 +1,14 @@
+//
+//  MoreMenuTap.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/06.
+//
+
+import Foundation
+
+enum MoreMenuTap {
+    case report
+    case cancel
+    case review
+}
