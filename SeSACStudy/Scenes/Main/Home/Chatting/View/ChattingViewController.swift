6ae2e621b450cdf2b85bd5631ae47730e91a47bc
skyler <ho971122@gmail.com>
2022-12-13 13:58:04 +0900

    Feat: 채팅 화면에서 더보기 버튼 클릭 시 내 상태 확인 통신 코드 추가

--- SeSACStudy/Scenes/Main/Home/Chatting/View/ChattingViewController.swift
+++ SeSACStudy/Scenes/Main/Home/Chatting/View/ChattingViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 import RxSwift
 import RxCocoa
+import FirebaseAuth
 import SnapKit
 
 class ChattingViewController: BaseViewController {
@@ -26,7 +27,7 @@ class ChattingViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         ChatRepository.shared.fetch()
+        ChatRepository.shared.fetch()
         //on chat으로 받은 이벤트를 처리하기 위한 Notification Observer
         NotificationCenter.default.addObserver(self, selector: #selector(getMessage), name: NSNotification.Name("getMessage"), object: nil)
     }
@@ -34,32 +35,117 @@ class ChattingViewController: BaseViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = true
         self.navigationController?.navigationBar.isHidden = false
+        
+        //MARK: - 소켓 연결
+        SocketIOManager.shared.establishConnection()
+        
+        //MARK: - 채팅 목록 불러오기
+        loadChat()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
+        //MARK: - 소켓 연결 해제
         SocketIOManager.shared.closeConnection()
     }
     
+    //MARK: - 채팅 목록 불러오기
+    func loadChat() {
+        APIService.loadChat { [weak self] (value, statusCode, error) in
+            guard let statusCode = statusCode else { return }
+            guard let status = NetworkError(rawValue: statusCode) else { return }
+            switch status {
+            case .success:
+                value?.payload.forEach { ChatRepository.shared.saveChat(item: ChatData(chatId: $0.id, toChat: $0.to, fromChat: $0.from, chatContent: $0.chat, chatDate: $0.createdAt)) }
+                self?.chatView.tableView.reloadData()
+                self?.chatView.tableView.scrollToRow(at: IndexPath(row: (ChatRepository.shared.tasks?.count ?? 0) - 1, section: 0), at: .bottom, animated: false)
+            case .invalidToken: self?.refreshToken1()
+            default: self?.showToast("잠시 후 다시 시도해주세요.")
+            }
+        }
+    }
+    
+    //MARK: - 토큰 만료 시 토큰 재발급
+    func refreshToken1() {
+        let currentUser = Auth.auth().currentUser
+        currentUser?.getIDTokenForcingRefresh(true) { token, error in
+            if let error = error {
+                self.showToast("에러: \(error.localizedDescription)")
+                return
+            } else if let token = token {
+                UserDefaultsManager.token = token
+                APIService.loadChat { [weak self] (value, statusCode, error) in
+                    guard let statusCode = statusCode else { return }
+                    guard let status = NetworkError(rawValue: statusCode) else { return }
+                    switch status {
+                    case .success:
+                        value?.payload.forEach { ChatRepository.shared.saveChat(item: ChatData(chatId: $0.id, toChat: $0.to, fromChat: $0.from, chatContent: $0.chat, chatDate: $0.createdAt)) }
+                        self?.chatView.tableView.reloadData()
+                        self?.chatView.tableView.scrollToRow(at: IndexPath(row: (ChatRepository.shared.tasks?.count ?? 0) - 1, section: 0), at: .bottom, animated: false)
+                    default: self?.showToast("잠시 후 다시 시도해주세요.")
+                    }
+                }
+            }
+        }
+    }
+    
     @objc func getMessage(notification: NSNotification) {
-            
-        let chat = notification.userInfo!["chat"] as! String
-        let name = notification.userInfo!["name"] as! String
-        let createdAt = notification.userInfo!["createdAt"] as! String
-        let userID = notification.userInfo!["userId"] as! String
+        let id = notification.userInfo![TextCase.Chatting.ChatSokcet.id.rawValue] as! String
+        let chat = notification.userInfo![TextCase.Chatting.ChatSokcet.chat.rawValue] as! String
+        let createdAt = notification.userInfo![TextCase.Chatting.ChatSokcet.createdAt.rawValue] as! String
+        let from = notification.userInfo![TextCase.Chatting.ChatSokcet.from.rawValue] as! String
+        let to = notification.userInfo![TextCase.Chatting.ChatSokcet.to.rawValue] as! String
         
+        let value = ChatData(chatId: id, toChat: to, fromChat: from, chatContent: chat, chatDate: createdAt)
         
-        
-        let value = Chat(text: chat, userID: userID, name: name, username: "", id: "", createdAt: createdAt, updatedAt: "", v: 0, ID: "")
-        
-        self.chat.append(value)
+        ChatRepository.shared.saveChat(item: value)
         chatView.tableView.reloadData()
-        chatView.tableView.scrollToRow(at: IndexPath(row: self.chat.count - 1, section: 0), at: .bottom, animated: false)
+        chatView.tableView.scrollToRow(at: IndexPath(row: (ChatRepository.shared.tasks?.count ?? 0) - 1, section: 0), at: .bottom, animated: false)
     }
     
     @objc func backHome() {
         self.navigationController?.popViewControllers(3)
+    }
+    
+    //MARK: - 현재 상태 확인
+    func updateMyState() {
+        APIService.myQueueState { [weak self] (value, statusCode, error) in
+            guard let statusCode = statusCode else { return }
+            guard let status = NetworkError(rawValue: statusCode) else { return }
+            switch status {
+            case .success:
+                if value?.matched == 1 { self?.chatView.cancelButton.setTitle(TextCase.Chatting.cancel.rawValue, for: .normal) }
+                if value?.dodged == 1 || value?.reviewed == 1 { self?.chatView.cancelButton.setTitle(TextCase.Chatting.quit.rawValue, for: .normal) }
+                self?.chatView.cancelButton.setTitleColor(BlackNWhite.black, for: .normal)
+            case .invalidToken: self?.refreshToken2()
+            default: self?.showToast("\(statusCode), 기타 에러")
+            }
+        }
+    }
+    
+    //MARK: - 토큰 만료 시 토큰 재발급
+    func refreshToken2() {
+        let currentUser = Auth.auth().currentUser
+        currentUser?.getIDTokenForcingRefresh(true) { token, error in
+            if let error = error {
+                self.showToast("에러: \(error.localizedDescription)")
+                return
+            } else if let token = token {
+                UserDefaultsManager.token = token
+                APIService.myQueueState { [weak self] (value, statusCode, error) in
+                    guard let statusCode = statusCode else { return }
+                    guard let status = NetworkError(rawValue: statusCode) else { return }
+                    switch status {
+                    case .success:
+                        if value?.matched == 1 { self?.chatView.cancelButton.setTitle("스터디 취소", for: .normal) }
+                        if value?.dodged == 1 || value?.reviewed == 1 { self?.chatView.cancelButton.setTitle("스터디 종료", for: .normal) }
+                        self?.chatView.cancelButton.setTitleColor(BlackNWhite.black, for: .normal)
+                    default: self?.showToast("잠시 후 다시 시도해주세요.")
+                    }
+                }
+            }
+        }
     }
     
     @objc func moreTapped() {
@@ -74,6 +160,9 @@ class ChattingViewController: BaseViewController {
                 $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.top)
             }
         } else {
+            //MARK: - 더보기 버튼 클릭 시 사용자 상태 확인
+            updateMyState()
+            
             chatView.moreMenuBack.isHidden = more
             
             chatView.moreMenu.isHidden = more
@@ -114,6 +203,27 @@ class ChattingViewController: BaseViewController {
     }
     
     override func bindData() {
+        
+        //MARK: - 새싹 신고
+        chatView.reportButton.rx.tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                vc.showToast("준비 중인 서비스입니다.")
+            }
+            .disposed(by: disposeBag)
+        
+        //MARK: - 스터디 취소
+        chatView.cancelButton.rx.tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                let viewController = CancelViewController()
+                viewController.type = vc.chatView.cancelButton.currentTitle ?? ""
+                viewController.modalPresentationStyle = .overFullScreen                
+                vc.present(viewController, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        //MARK: - 리뷰 등록
         chatView.reviewButton.rx.tap
             .withUnretained(self)
             .bind { (vc, _) in
