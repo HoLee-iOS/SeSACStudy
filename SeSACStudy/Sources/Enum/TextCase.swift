f7d5e41bf56e5c0af15f8d039fb97d2b8b6b0c71
skyler <ho971122@gmail.com>
2022-12-13 14:06:52 +0900

    Feat: TextCase 열거형에 소켓에서 사용할 새로운 열거형 추가

--- SeSACStudy/Sources/Enum/TextCase.swift
+++ SeSACStudy/Sources/Enum/TextCase.swift
@@ -14,9 +14,17 @@ enum TextCase {
         case report = "새싹 신고"
         case cancel = "스터디 취소"
         case review = "리뷰 등록"
+        case quit = "스터디 종료"
         case reviewButton = "리뷰 등록하기"
-        case reviewPlaceholder = "자세한 피드백은 다른 새싹들에게 도움이 됩니다\n(500자 이내 작성)"
-        case defaultLastDate = "2000-01-01T00:00:00.000Z"
+        case reviewPlaceholder = " 자세한 피드백은 다른 새싹들에게 도움이 됩니다\n (500자 이내 작성)"
+        enum ChatSokcet: String {
+            case id = "_id"
+            case chat = "chat"
+            case createdAt = "createdAt"
+            case from = "from"
+            case to = "to"
+            case defaultLastDate = "2000-01-01T00:00:00.000Z"
+        }
     }
     enum Review: String {
         case manner = "좋은 매너"
@@ -25,6 +33,5 @@ enum TextCase {
         case kind = "친절한 성격"
         case skill = "능숙한 실력"
         case beneficial = "유익한 시간"
-        case question = "고래밥님과의 스터디는 어떠셨나요?"
     }
 }
