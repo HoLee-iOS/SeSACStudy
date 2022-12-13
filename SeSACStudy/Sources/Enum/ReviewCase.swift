25a3df80a66a7742862e20301e4e33434680576e
skyler <ho971122@gmail.com>
2022-12-13 16:34:23 +0900

    Feat: Review 버튼 열거형 케이스 생성

---
+++ SeSACStudy/Sources/Enum/ReviewCase.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewCase.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/13.
+//
+
+import Foundation
+
+enum ReviewCase: Int {
+    case manner
+    case promise
+    case response
+    case kind
+    case skill
+    case beneficial
+}
