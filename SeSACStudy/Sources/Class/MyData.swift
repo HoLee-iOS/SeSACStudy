1a50ecec9a3371d5703a73199198500dc5449841
skyler <ho971122@gmail.com>
2022-12-15 17:55:12 +0900

    Feat: 로그인 통신 시에 필요한 정보를 저장할 싱글턴 클래스 생성

---
+++ SeSACStudy/Sources/Class/MyData.swift
@@ -0,0 +1,18 @@
+//
+//  MyData.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/15.
+//
+
+import Foundation
+
+class MyData {
+    
+    private init() {}
+    
+    static let shared = MyData()
+    
+    var sesac = 0
+    var background = 0
+}
