432cce8d5a6a5e188eae97fb0b231a33d00d270e
skyler <ho971122@gmail.com>
2022-12-16 03:52:05 +0900

    Feat: 새싹샵 통신 시 데이터 담을 싱글턴 클래스 생성

---
+++ SeSACStudy/Utilities/Class/ShopDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  ShopDataModel.swift
+//  SeSACStudy
+//
+//  Created by 이현호 on 2022/12/16.
+//
+
+import Foundation
+
+class ShopDataModel {
+    
+    private init() {}
+    
+    static let shared = ShopDataModel()
+    
+    var sesac = 0
+    var background = 0
+}
