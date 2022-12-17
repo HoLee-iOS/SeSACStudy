d0b6b32daf03f5acc5ce8269506a275a68b79198
skyler <ho971122@gmail.com>
2022-12-17 18:10:06 +0900

    Feat: 버튼 클릭 시 인앱 결제 구매 진행 후 적용 코드 추가

--- SeSACStudy/Scenes/Shop/Cell/BackgroundCollectionViewCell.swift
+++ SeSACStudy/Scenes/Shop/Cell/BackgroundCollectionViewCell.swift
@@ -11,12 +11,15 @@ import FirebaseAuth
 import RxSwift
 import RxCocoa
 import SnapKit
+import StoreKit
 
 class BackgroundCollectionViewCell: BaseCollectionViewCell {
     
     let disposeBag = DisposeBag()
     
-    var index = 0
+    var productArr: [SKProduct] = []
+    
+    var products: SKProduct?
     
     let sesacBackground: UIImageView = {
         let view = UIImageView()
@@ -88,7 +91,7 @@ class BackgroundCollectionViewCell: BaseCollectionViewCell {
             .bind { (cell, _) in
                 cell.loadMyInfo()
                 if cell.priceButton.currentAttributedTitle != NSAttributedString(string: TextCase.Shop.mine.rawValue, attributes: [NSAttributedString.Key.font: UIFont(name: Fonts.medium, size: 12)!]) {
-                    cell.inApp()
+                    cell.purchaseStart(product: cell.productArr[cell.priceButton.tag - 1])
                 }
             }
             .disposed(by: disposeBag)
@@ -104,6 +107,31 @@ class BackgroundCollectionViewCell: BaseCollectionViewCell {
             priceButton.backgroundColor = BrandColor.green
         }
     }
+    
+    //MARK: - 인앱결제 시작
+    func purchaseStart(product: SKProduct) {
+        let payment = SKPayment(product: product)
+        SKPaymentQueue.default().add(payment)
+        SKPaymentQueue.default().add(self)
+    }
+    
+    //MARK: - 영수증 검증
+    func receiptValidation(transaction: SKPaymentTransaction, productIdentifier: String) {
+        // SandBox: “https://sandbox.itunes.apple.com/verifyReceipt”
+        // iTunes Store : “https://buy.itunes.apple.com/verifyReceipt”
+        
+        //구매 영수증 정보
+        let receiptFileURL = Bundle.main.appStoreReceiptURL
+        let receiptData = try? Data(contentsOf: receiptFileURL!)
+        let receiptString = receiptData?.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
+        
+        ShopDataModel.shared.receipt = receiptString ?? ""
+        ShopDataModel.shared.product = productIdentifier
+        
+        //거래 내역(transaction)을 큐에서 제거
+        SKPaymentQueue.default().finishTransaction(transaction)
+        self.inApp()
+    }
 }
 
 extension BackgroundCollectionViewCell {
@@ -113,7 +141,7 @@ extension BackgroundCollectionViewCell {
             guard let statusCode = statusCode else { return }
             guard let status = NetworkError(rawValue: statusCode) else { return }
             switch status {
-            case .success: self?.inputData(index: self?.index ?? 0)
+            case .success: self?.inputData(index: self?.priceButton.tag ?? 0)
             case .invalidToken: self?.refreshToken1()
             default: self?.makeToast("잠시 후 다시 시도해 주세요.")
             }
@@ -136,7 +164,7 @@ extension BackgroundCollectionViewCell {
                     guard let statusCode = statusCode else { return }
                     guard let status = NetworkError(rawValue: statusCode) else { return }
                     switch status {
-                    case .success: self?.inputData(index: self?.index ?? 0)
+                    case .success: self?.inputData(index: self?.priceButton.tag ?? 0)
                     default: self?.makeToast("잠시 후 다시 시도해 주세요.")
                     }
                 }
@@ -150,8 +178,7 @@ extension BackgroundCollectionViewCell {
             guard let statusCode = statusCode else { return }
             guard let status = NetworkError(rawValue: statusCode) else { return }
             switch status {
-            case .success: print("아이템 구매 성공")
-            case .alreadySignUp: print("영수증 검증실패")
+            case .success: self?.loadMyInfo()
             case .invalidToken: self?.refreshToken2()
             default: self?.makeToast("잠시 후 다시 시도해 주세요.")
             }
@@ -174,12 +201,41 @@ extension BackgroundCollectionViewCell {
                     guard let statusCode = statusCode else { return }
                     guard let status = NetworkError(rawValue: statusCode) else { return }
                     switch status {
-                    case .success: print("아이템 구매 성공")
-                    case .alreadySignUp: print("영수증 검증실패")
+                    case .success: self?.loadMyInfo()
                     default: self?.makeToast("잠시 후 다시 시도해 주세요.")
                     }
                 }
             }
         }
     }
+}
+
+extension BackgroundCollectionViewCell: SKPaymentTransactionObserver {
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        for transaction in transactions {
+            
+            switch transaction.transactionState {
+                
+            case .purchased: //구매 승인 이후에 영수증 검증
+                
+                print("Transaction Approved. \(transaction.payment.productIdentifier)")
+                receiptValidation(transaction: transaction, productIdentifier: transaction.payment.productIdentifier)
+                
+            case .failed: //실패 토스트, transaction
+                
+                print("Transaction Failed")
+                SKPaymentQueue.default().finishTransaction(transaction)
+                
+            default:
+                break
+            }
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, removedTransactions transactions: [SKPaymentTransaction]) {
+        print("removedTransactions")
+    }
+    
 }
--- SeSACStudy/Scenes/Shop/Cell/SesacCollectionViewCell.swift
+++ SeSACStudy/Scenes/Shop/Cell/SesacCollectionViewCell.swift
@@ -11,12 +11,13 @@ import FirebaseAuth
 import RxSwift
 import RxCocoa
 import SnapKit
+import StoreKit
 
 class SesacCollectionViewCell: BaseCollectionViewCell {
     
     let disposeBag = DisposeBag()
     
-    var index = 0
+    var productArr: [SKProduct] = []
     
     let sesacFace: UIImageView = {
         let view = UIImageView()
@@ -82,7 +83,7 @@ class SesacCollectionViewCell: BaseCollectionViewCell {
             .bind { (cell, _) in
                 cell.loadMyInfo()
                 if cell.priceButton.currentAttributedTitle != NSAttributedString(string: TextCase.Shop.mine.rawValue, attributes: [NSAttributedString.Key.font: UIFont(name: Fonts.medium, size: 12)!]) {
-                    cell.inApp()
+                    cell.purchaseStart(product: cell.productArr[cell.priceButton.tag + 6])
                 }
             }
             .disposed(by: disposeBag)
@@ -99,17 +100,40 @@ class SesacCollectionViewCell: BaseCollectionViewCell {
         }
     }
     
+    //MARK: - 인앱결제 시작
+    func purchaseStart(product: SKProduct) {
+        let payment = SKPayment(product: product)
+        SKPaymentQueue.default().add(payment)
+        SKPaymentQueue.default().add(self)
+    }
     
+    //MARK: - 영수증 검증
+    func receiptValidation(transaction: SKPaymentTransaction, productIdentifier: String) {
+        // SandBox: “https://sandbox.itunes.apple.com/verifyReceipt”
+        // iTunes Store : “https://buy.itunes.apple.com/verifyReceipt”
+        
+        //구매 영수증 정보
+        let receiptFileURL = Bundle.main.appStoreReceiptURL
+        let receiptData = try? Data(contentsOf: receiptFileURL!)
+        let receiptString = receiptData?.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
+        
+        ShopDataModel.shared.receipt = receiptString ?? ""
+        ShopDataModel.shared.product = productIdentifier
+        //거래 내역(transaction)을 큐에서 제거
+        SKPaymentQueue.default().finishTransaction(transaction)
+        self.inApp()
+    }
 }
 
 extension SesacCollectionViewCell {
+    
     //MARK: - 내 정보 불러오기
     func loadMyInfo() {
         APIService.requestMyInfo { [weak self] (value, statusCode, error) in
             guard let statusCode = statusCode else { return }
             guard let status = NetworkError(rawValue: statusCode) else { return }
             switch status {
-            case .success: self?.inputData(index: self?.index ?? 0)
+            case .success: self?.inputData(index: self?.priceButton.tag ?? 0)
             case .invalidToken: self?.refreshToken1()
             default: self?.makeToast("잠시 후 다시 시도해 주세요.")
             }
@@ -132,7 +156,7 @@ extension SesacCollectionViewCell {
                     guard let statusCode = statusCode else { return }
                     guard let status = NetworkError(rawValue: statusCode) else { return }
                     switch status {
-                    case .success: self?.inputData(index: self?.index ?? 0)
+                    case .success: self?.inputData(index: self?.priceButton.tag ?? 0)
                     default: self?.makeToast("잠시 후 다시 시도해 주세요.")
                     }
                 }
@@ -146,8 +170,7 @@ extension SesacCollectionViewCell {
             guard let statusCode = statusCode else { return }
             guard let status = NetworkError(rawValue: statusCode) else { return }
             switch status {
-            case .success: print("아이템 구매 성공")
-            case .alreadySignUp: print("영수증 검증실패")
+            case .success: self?.loadMyInfo()
             case .invalidToken: self?.refreshToken2()
             default: self?.makeToast("잠시 후 다시 시도해 주세요.")
             }
@@ -170,8 +193,7 @@ extension SesacCollectionViewCell {
                     guard let statusCode = statusCode else { return }
                     guard let status = NetworkError(rawValue: statusCode) else { return }
                     switch status {
-                    case .success: print("아이템 구매 성공")
-                    case .alreadySignUp: print("영수증 검증실패")
+                    case .success: self?.loadMyInfo()
                     default: self?.makeToast("잠시 후 다시 시도해 주세요.")
                     }
                 }
@@ -179,3 +201,34 @@ extension SesacCollectionViewCell {
         }
     }
 }
+
+extension SesacCollectionViewCell: SKPaymentTransactionObserver {
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        for transaction in transactions {
+            
+            switch transaction.transactionState {
+                
+            case .purchased: //구매 승인 이후에 영수증 검증
+                
+                print("Transaction Approved. \(transaction.payment.productIdentifier)")
+                receiptValidation(transaction: transaction, productIdentifier: transaction.payment.productIdentifier)
+                
+            case .failed: //실패 토스트, transaction
+                
+                print("Transaction Failed")
+                SKPaymentQueue.default().finishTransaction(transaction)
+                
+            default:
+                break
+            }
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, removedTransactions transactions: [SKPaymentTransaction]) {
+        print("removedTransactions")
+    }
+    
+}
+
